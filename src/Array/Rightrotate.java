8a818ff8ca648a6ae1f3e77f70e47e5bdb5fd759
Bhavana <bhavananb2003@gmail.com>
2024-10-02 01:21:32 +0530

    rotate an array

---
+++ src/Array/Rightrotate.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class Rightrotate {
+
+    public static void rightRotateArray(int[] arr,int k) {
+        int n = arr.length;
+        k = k % n;
+
+        rotate(arr,0,n-1);
+        rotate(arr,0,k-1);
+        rotate(arr,k,n-1);
+
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void rotate(int[] arr,int start,int end) {
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        rightRotateArray(arr,3);
+    }
+}
